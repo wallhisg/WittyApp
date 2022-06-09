d8d61d35a1b786fea193edbaed00af2af61ea7f1
wallhisg <wallhisg@gmail.com>
2022-06-09 22:58:16 +0700

    Init app.

---
+++ unitTest/SAMPLE/WEB_SERVER_01/WS_Hello.cpp
@@ -0,0 +1,25 @@
+
+#include <HelloApplication.h>
+
+
+int main(int argc, char **argv)
+{
+  /*
+   * Your main method may set up some shared resources, but should then
+   * start the server application (FastCGI or httpd) that starts listening
+   * for requests, and handles all of the application life cycles.
+   *
+   * The last argument to WRun specifies the function that will instantiate
+   * new application objects. That function is executed when a new user surfs
+   * to the Wt application, and after the library has negotiated browser
+   * support. The function should return a newly instantiated application
+   * object.
+   */
+  return Wt::WRun(argc, argv, [](const Wt::WEnvironment &env) {
+    /*
+     * You could read information from the environment to decide whether
+     * the user has permission to start a new application
+     */
+    return std::make_unique<HelloApplication>(env);
+  });
+}
